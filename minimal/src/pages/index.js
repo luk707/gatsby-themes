a68fb70a3b3f81bc28677acd9471753a334859b0
Luke Harris <lukeharris01170@gmail.com>
2019-01-17 22:08:37 +0000

    Initial commit

---
+++ minimal/src/pages/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Button } from "@luk707/components";
+
+class HomePage extends Component {
+  render() {
+    return <Button>Hello World!</Button>;
+  }
+}
+
+export default HomePage;
